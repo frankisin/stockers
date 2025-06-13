5e4b16d514fda5d31b8b91923e2d277af27a8c1b
frankisinny <frank.velazquez@rockhopper.tech>
2025-06-13 15:50:53 -0400

    moved dockerfile

--- Stockers.API/Stockers.API/obj/Debug/net8.0/Stockers.API.AssemblyInfo.cs
+++ Stockers.API/Stockers.API/obj/Debug/net8.0/Stockers.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stockers.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de3e5f4541fff96b4623431a3872a351060f2516")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8213281db40cde2d7f01097e2c30da94a47123fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stockers.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stockers.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
