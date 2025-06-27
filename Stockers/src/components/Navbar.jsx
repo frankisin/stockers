541948a116f339fc9bfc6de29805c5ff038300ed
Frank Velazquez <velazuezfra96@gmail.com>
2025-06-27 06:05:32 -0400

    final fixes

--- Stockers/src/components/Navbar.jsx
+++ Stockers/src/components/Navbar.jsx
@@ -17,56 +17,57 @@ import { usePopover } from '@/hooks/use-popover';
 import { UserPopover } from '../../src/components/dashboard/layout/user-popover';
 
 function Navbar() {
-  const userPopover = usePopover();
+    const userPopover = usePopover();
 
-  return (
-    <header>
-      <nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
-        <div className="container px-5">
-          <a className="navbar-brand" href="/home">Stockers</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
-            aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+                <div className="container px-5">
+                    <a className="navbar-brand" href="/home">Stockers</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+                        aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-          <div className="collapse navbar-collapse" id="navbarResponsive">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item"><a className="nav-link" href="/register">Register</a></li>
-              <li className="nav-item"><a className="nav-link" href="/login">Log In</a></li>
-            </ul>
+                    <div className="collapse navbar-collapse" id="navbarResponsive">
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item"><a className="nav-link" href="/register">Register</a></li>
+                            <li className="nav-item"><a className="nav-link" href="/login">Log In</a></li>
+                        </ul>
 
-            {/* RIGHT SIDE ICONS */}
-            <Stack direction="row" spacing={2} sx={{ alignItems: 'center', ml: 2 }}>
-              <Tooltip title="Contacts">
-                <IconButton>
-                  <UsersIcon />
-                </IconButton>
-              </Tooltip>
-              <Tooltip title="Notifications">
-                <Badge badgeContent={4} color="success" variant="dot">
-                  <IconButton>
-                    <BellIcon />
-                  </IconButton>
-                </Badge>
-              </Tooltip>
-              <Avatar
-                onClick={userPopover.handleOpen}
-                ref={userPopover.anchorRef}
-                src="/assets/avatar.png"
-                sx={{ cursor: 'pointer' }}
-              />
-            </Stack>
-          </div>
-        </div>
-      </nav>
+                        {/* RIGHT SIDE ICONS */}
+                        <Stack direction="row" spacing={2} sx={{ alignItems: 'center', ml: 2, color: 'white' }}>
+                            <Tooltip title="Contacts">
+                                <IconButton color="inherit">
+                                    <UsersIcon />
+                                </IconButton>
+                            </Tooltip>
+                            <Tooltip title="Notifications">
+                                <Badge badgeContent={4} color="success" variant="dot">
+                                    <IconButton color="inherit">
+                                        <BellIcon />
+                                    </IconButton>
+                                </Badge>
+                            </Tooltip>
+                            <Avatar
+                                onClick={userPopover.handleOpen}
+                                ref={userPopover.anchorRef}
+                                src="/assets/avatar.png"
+                                sx={{ cursor: 'pointer' }}
+                            />
+                        </Stack>
 
-      <UserPopover
-        anchorEl={userPopover.anchorRef.current}
-        onClose={userPopover.handleClose}
-        open={userPopover.open}
-      />
-    </header>
-  );
+                    </div>
+                </div>
+            </nav>
+
+            <UserPopover
+                anchorEl={userPopover.anchorRef.current}
+                onClose={userPopover.handleClose}
+                open={userPopover.open}
+            />
+        </header>
+    );
 }
 
 export default Navbar;
