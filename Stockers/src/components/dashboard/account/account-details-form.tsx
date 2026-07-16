a901f54fcd374b5a7f179501325f1055cd5452f0
Frank Velazquez <velazuezfra96@gmail.com>
2026-07-16 06:35:46 -0400

    finished account form

--- Stockers/src/components/dashboard/account/account-details-form.tsx
+++ Stockers/src/components/dashboard/account/account-details-form.tsx
@@ -16,10 +16,56 @@ import Select from '@mui/material/Select';
 import { useUserContext } from '../../../contexts/user-context';
 
 const states = [
-  { value: 'alabama', label: 'Alabama' },
-  { value: 'new-york', label: 'New York' },
-  { value: 'san-francisco', label: 'San Francisco' },
-  { value: 'los-angeles', label: 'Los Angeles' },
+  { value: 'Alabama', label: 'Alabama' },
+  { value: 'Alaska', label: 'Alaska' },
+  { value: 'Arizona', label: 'Arizona' },
+  { value: 'Arkansas', label: 'Arkansas' },
+  { value: 'California', label: 'California' },
+  { value: 'Colorado', label: 'Colorado' },
+  { value: 'Connecticut', label: 'Connecticut' },
+  { value: 'Delaware', label: 'Delaware' },
+  { value: 'Florida', label: 'Florida' },
+  { value: 'Georgia', label: 'Georgia' },
+  { value: 'Hawaii', label: 'Hawaii' },
+  { value: 'Idaho', label: 'Idaho' },
+  { value: 'Illinois', label: 'Illinois' },
+  { value: 'Indiana', label: 'Indiana' },
+  { value: 'Iowa', label: 'Iowa' },
+  { value: 'Kansas', label: 'Kansas' },
+  { value: 'Kentucky', label: 'Kentucky' },
+  { value: 'Louisiana', label: 'Louisiana' },
+  { value: 'Maine', label: 'Maine' },
+  { value: 'Maryland', label: 'Maryland' },
+  { value: 'Massachusetts', label: 'Massachusetts' },
+  { value: 'Michigan', label: 'Michigan' },
+  { value: 'Minnesota', label: 'Minnesota' },
+  { value: 'Mississippi', label: 'Mississippi' },
+  { value: 'Missouri', label: 'Missouri' },
+  { value: 'Montana', label: 'Montana' },
+  { value: 'Nebraska', label: 'Nebraska' },
+  { value: 'Nevada', label: 'Nevada' },
+  { value: 'New Hampshire', label: 'New Hampshire' },
+  { value: 'New Jersey', label: 'New Jersey' },
+  { value: 'New Mexico', label: 'New Mexico' },
+  { value: 'New York', label: 'New York' },
+  { value: 'North Carolina', label: 'North Carolina' },
+  { value: 'North Dakota', label: 'North Dakota' },
+  { value: 'Ohio', label: 'Ohio' },
+  { value: 'Oklahoma', label: 'Oklahoma' },
+  { value: 'Oregon', label: 'Oregon' },
+  { value: 'Pennsylvania', label: 'Pennsylvania' },
+  { value: 'Rhode Island', label: 'Rhode Island' },
+  { value: 'South Carolina', label: 'South Carolina' },
+  { value: 'South Dakota', label: 'South Dakota' },
+  { value: 'Tennessee', label: 'Tennessee' },
+  { value: 'Texas', label: 'Texas' },
+  { value: 'Utah', label: 'Utah' },
+  { value: 'Vermont', label: 'Vermont' },
+  { value: 'Virginia', label: 'Virginia' },
+  { value: 'Washington', label: 'Washington' },
+  { value: 'West Virginia', label: 'West Virginia' },
+  { value: 'Wisconsin', label: 'Wisconsin' },
+  { value: 'Wyoming', label: 'Wyoming' },
 ] as const;
 
 
@@ -78,7 +124,7 @@ export function AccountDetailsForm(): React.JSX.Element {
             >
               <FormControl fullWidth>
                 <InputLabel>Phone number</InputLabel>
-                <OutlinedInput label="Phone number" name="phone" type="tel" />
+                <OutlinedInput defaultValue={user?.phoneNumber ?? ''} label="Phone Number" name="phone" />
               </FormControl>
             </Grid>
             <Grid
@@ -89,7 +135,12 @@ export function AccountDetailsForm(): React.JSX.Element {
             >
               <FormControl fullWidth>
                 <InputLabel>State</InputLabel>
-                <Select defaultValue="New York" label="State" name="state" variant="outlined">
+                <Select
+                  defaultValue={user?.state ?? ''}
+                  label="State"
+                  name="state"
+                  variant="outlined"
+                >
                   {states.map((option) => (
                     <MenuItem key={option.value} value={option.value}>
                       {option.label}
