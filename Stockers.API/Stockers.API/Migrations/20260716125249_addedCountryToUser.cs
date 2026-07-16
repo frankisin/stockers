0e583ba2fe5f34e37ceb9d824957327dcc5f3905
Frank Velazquez <velazuezfra96@gmail.com>
2026-07-16 09:29:31 -0400

    added country field to user

---
+++ Stockers.API/Stockers.API/Migrations/20260716125249_addedCountryToUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stockers.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedCountryToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "country",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "country",
+                table: "users");
+        }
+    }
+}
